9a5c56d1ce142bdc86bfa09a23a30c3734d5f2cd
zouhourkharraf <zouhour.kharraf1@esprit.tn>
2022-10-03 05:58:06 +0100

    atelier3

---
+++ src/Controller/ClubController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClubController extends AbstractController
+{
+    #[Route('/club',name: 'app_club')]
+    public function index():Response
+    {
+        return $this->render('club/index.html.twig',['controller_name' => 'ClubController']);
+    }
+
+    #[Route('/listFormation', name: 'list_formation')]
+    public function listFormation()
+    {
+        $var1="3A32";
+        $var2="J12";
+        $tab_formations=array(array('ref' => 'form147', 'Titre' => 'Formation Symfony4','Description'=>'formation pratique','date_debut'=>'12/06/2020', 'date_fin'=>'19/06/2020','nb_participants'=>19),
+        array('ref'=>'form177','Titre'=>'Formation SOA','Description'=>'formation theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020','nb_participants'=>0),
+        array('ref'=>'form178','Titre'=>'Formation Angular','Description'=>'formation theorique','date_debut'=>'10/06/2020','date_fin'=>'14/06/2020','nb_participants'=>12));
+
+        return $this->render("club/list.html.twig",array("tableau_formations"=>$tab_formations));
+        
+    }
+
+    #[Route('/getname', name: 'get_name')]
+    public function getName(string $nom)
+    {
+
+      return $this->render("/club/get{$nom}");
+    }
+
+
+    
+}
+
+
+
+?>
