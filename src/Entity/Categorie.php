b9491b8c6babadc75ae07ebb5f797d42c71981a8
zouhourkharraf <zouhour.kharraf1@esprit.tn>
2022-10-03 12:04:47 +0100

    seance4

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\Column(length: 100)]
+    private ?string $reference = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $titre = null;
+
+  
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+}
